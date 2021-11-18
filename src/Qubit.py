eb92345a93bc5ee51783a7f85676f35343972f41
ss4774 <ss4774@student.uni-lj.si>
2021-11-18 18:48:55 +0100

    Added basic qubit representation

---
+++ src/Qubit.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Qubit():
+    def __init__(self, state0=None, state1=None):
+        self.state0 = state0
+        self.state1 = state1
+        self.vector = np.array([state0, state1])
+
+    def __str__(self):
+        return "Qubit: " + str(self.vector)
+
+    def normalize(self):
+        norm = np.linalg.norm(self.vector)
+        self.vector = self.vector / norm
+        return self
