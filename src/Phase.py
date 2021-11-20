617b2120638eb5ea9a76ec8756c4ed281cb4bbb3
ss4774 <ss4774@student.uni-lj.si>
2021-11-20 16:25:59 +0100

    Updated eval of a phase

--- src/Phase.py
+++ src/Phase.py
@@ -1,4 +1,4 @@
-from src.Gate import Gate
+from src.Gate import Gate, I
 
 import numpy as np
 from itertools import islice
@@ -9,7 +9,7 @@ class Phase():
     def __init__(self, size):
         self.size = size
         self.gates = size * [None]
-        self.mat = None
+        self.mat = np.array([1])
 
     def __str__(self):
         return "-".join(map(lambda g: g.label, self.gates))
@@ -18,17 +18,22 @@ class Phase():
         for i in range(gate.inputSize):
             self.gates[location + i] = gate
 
+    def getNormalizedGates(self):
+        normalizedGates = list()
+        gatesIterator = iter(self.gates)
+
+        for gate in gatesIterator:
+            if gate is None:
+                normalizedGates.append(I())
+            else:
+                normalizedGates.append(gate)
+                _ = list(islice(gatesIterator, gate.inputSize - 1))
+
+        return normalizedGates
+
     def getMaxLableLength(self):
         return reduce(lambda acc, gate: max(acc, 1 if gate is None else len(gate.label)), self.gates, 0)
 
-    #TODO fix
     def eval(self):
-        gatesIterator = iter(self.gates)
-        gate = next(gatesIterator)
-
-        self.mat = gate.mat
-        gatesIterator = islice(gatesIterator, gate.inputSize - 1, None)
-
-        for gate in gatesIterator:
+        for gate in self.getNormalizedGates():
             self.mat = np.kron(self.mat, gate.mat)
-            gatesIterator = islice(gatesIterator, gate.inputSize - 1, None)
